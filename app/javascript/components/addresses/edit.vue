8cb606d3c015c7e15801d743df5814ab929e9f53
Cedric Wider <wider.cedric@gmail.com>
2019-03-25 14:28:10 +0100

    Add Loading indicator for edit action

--- app/javascript/components/addresses/edit.vue
+++ app/javascript/components/addresses/edit.vue
@@ -1,8 +1,57 @@
 <template>
+  <section class="patient-edit">
+    <c-loading :loading="isLoading">
+      <patient-form
+        :patient="patient"
+        :error-message="errorMessage"
+        :error="serverError"
+        @save="savePatient"
+        @cancel="showPatient"
+      />
+    </c-loading>
+  </section>
 </template>
 
 <script>
+import { mapActions, mapGetters } from 'vuex';
+import PatientForm from './patient_form.vue';
+import { patientMixin } from '../../mixins/patient_mixin';
+import { patients } from '../../store/types';
+import CLoading from '../shared/c_loading.vue';
+
+export default {
+  components: {
+    PatientForm,
+    CLoading,
+  },
+  mixins: [patientMixin],
+  computed: {
+    ...mapGetters({ isLoading: patients.loading }),
+  },
+  data() {
+    return {
+      serverError: null,
+      patient: { address: {} },
+    };
+  },
+  created() {
+    this.loadPatient(this.$route.params.id)
+      .then((patient) => {
+        this.patient = patient;
+        this.setCurrentPatient(patient);
+      })
+      .catch((serverError) => {
+        this.serverError = serverError;
+        console.log('Error while loading patient: ', serverError);
+      });
+  },
+  methods: {
+    ...mapActions({ loadPatient: patients.show, setCurrentPatient: patients.current }),
+    showPatient() {
+      this.$router.push({ name: 'addressshow', params: { id: this.patient.id } });
+    },
+  },
+};
 </script>
 
-<style lang="scss" scoped>
-</style>
+<style lang="scss" scoped></style>
