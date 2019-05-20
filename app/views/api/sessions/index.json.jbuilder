c092b0f87cd36eb3bc1d0c3dd72940af0de4554b
Cedric Wider <wider.cedric@gmail.com>
2019-05-20 15:00:34 +0200

    Introduce SessionController

---
+++ app/views/api/sessions/index.json.jbuilder
@@ -0,0 +1,1 @@
+json.array! @sessions, partial: 'api/sessions/session', as: :session
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
     resources :patients, only: [:index, :show, :create, :update, :destroy]
     resources :addresses, only: [:show, :create, :update, :destroy]
     resources :therapies, only: [:index, :show, :create, :update, :destroy]
+    resources :sessions, only: [:index, :show, :create, :update, :destroy]
   end
 
   get '*unmatched_route', to: 'home#index'
