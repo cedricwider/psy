5f6b155f843a4e6649406e72fd54ebdae76fc91c
Cedric Wider <wider.cedric@gmail.com>
2019-05-27 17:03:48 +0200

    Add all GUI elements on the page
    Form is somewhat usable. Cancel and save functionality has yet to be
    implemented.

--- app/javascript/components/sessions/session_form.vue
+++ app/javascript/components/sessions/session_form.vue
@@ -11,38 +11,69 @@
         <b-select
           placeholder="Therapie auswählen..."
           :loading="therapiesLoading"
+          @input="onTherapySelected"
         >
           <option
             v-for="therapy in allTherapies"
             :key="therapy.id"
-            :value="therapy.id"
+            :value="therapy"
           >
             {{ therapyAsOptionString(therapy) }}
           </option>
         </b-select>
       </b-field>
-
-      <!-- Date -->
-      <b-field label="Datum">
-        <b-datepicker
-          v-model="startDate"
-          placeholder="Datum auswählen..."
-          icon="calendar-today"
-        />
-      </b-field>
-
-      <!-- Duration -->
-      <b-field label="Dauer">
-        <b-select placeholder="Dauer auswählen...">
-          <option
-            v-for="duration in durations"
-            :key="duration"
-            :value="duration"
-          >
-            {{ duration }}
-          </option>
-        </b-select>
-      </b-field>
+      <div class="columns">
+        <div class="column is-one-third">
+          <!-- Date -->
+          <b-field label="Datum">
+            <b-datepicker
+              v-model="startDate"
+              placeholder="Datum auswählen..."
+              icon="calendar-today"
+            />
+          </b-field>
+        </div>
+        <div class="column is-one-third">
+          <!-- Duration -->
+          <b-field label="Dauer">
+            <b-select placeholder="Dauer auswählen...">
+              <option
+                v-for="duration in durations"
+                :key="duration"
+                :value="duration"
+              >
+                {{ duration }}
+              </option>
+            </b-select>
+          </b-field>
+        </div>
+        <div class="column is-one-third">
+          <!-- Price -->
+          <b-field label="Preis">
+            <b-numberinput
+              v-model="sessionPrice"
+              step="10"
+            />
+          </b-field>
+        </div>
+      </div>
+      <template slot="buttons">
+        <button
+          id="cancel"
+          class="button is-outlined"
+          @click="onCancelButtonClicked"
+        >
+          Abbrechen
+        </button>
+        <button
+          id="submit"
+          class="button is-primary"
+          type="submit"
+          @click="onSaveButtonClicked"
+        >
+          Speichern
+        </button>
+      </template>
     </form-layout>
   </section>
 </template>
@@ -63,7 +94,10 @@ export default {
       durations: [1, 2, 3, 4, 5],
       error: null,
       errorMessage: '',
+      sessionPrice: 0,
       startDate: new Date(),
+      therapyId: null,
+      duration: null,
     };
   },
   computed: {
@@ -78,6 +112,16 @@ export default {
     }),
     therapyAsOptionString(therapy) {
       return `${therapy.title} (${therapy.patients.map(p => p.lastName).join(', ')})`;
+    },
+    onTherapySelected(therapy) {
+      this.therapyId = therapy.id;
+      this.sessionPrice = therapy.price;
+    },
+    onSaveButtonClicked() {
+      console.log('Save button clicked. Please implement some logic');
+    },
+    onCancelButtonClicked() {
+      console.log('Cancel button clicked. Please implement some logic');
     },
   },
 };
